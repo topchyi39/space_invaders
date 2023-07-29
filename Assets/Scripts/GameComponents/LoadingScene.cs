d66475ae23514cf5868e66ec1763ba1290a327ae
Mykyta Topchyi <topchyi39@gmail.com>
2023-07-29 18:19:27 +0300

    Add logo screen

---
+++ Assets/Scripts/GameComponents/LoadingScene.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GameComponents
+{
+    public class LoadingScene : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup canvasGroup;
+        [SerializeField] private Transform target;
+        [SerializeField] private float duration;
+        
+
+        private IEnumerator Start()
+        {
+            target.DOScale(1.2f, duration);
+            canvasGroup.DOFade(1f, duration);
+            yield return new WaitForSeconds(duration / 2f);
+            SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+        }
+    }
+}
